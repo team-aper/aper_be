5e92fe56d30ff8dd9743d6797e39236fa6660626
qkrckstjq <cksk49@naver.com>
2024-07-10 15:15:13 +0900

    feat: init commit

---
+++ src/main/java/org/example/springaper/SpringAperApplication.java
@@ -0,0 +1,13 @@
+package org.example.springaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springaper/SpringAperApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springaper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
