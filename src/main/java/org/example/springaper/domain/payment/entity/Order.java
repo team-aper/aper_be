76038d52f8d68df4528a2a1412cabe528c7ead57
qkrckstjq <cksk49@naver.com>
2024-07-12 22:11:00 +0900

    feat: order 앤티티 생성

---
+++ src/main/java/org/example/springaper/domain/payment/entity/Order.java
@@ -0,0 +1,32 @@
+package org.example.springaper.domain.payment.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.example.springaper.domain.user.entity.User;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long orderId;
+
+    @Column
+    private Long totalAmount;
+
+    @OneToMany(mappedBy = "userId")
+    private List<User> user;
+
+    @OneToOne(mappedBy = "paymentId")
+    private PaymentId paymentId;
+
+    @Column(name = "order_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime orderDate;
+}
