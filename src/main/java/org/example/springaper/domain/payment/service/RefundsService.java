5a2d8024977a3a97c3fd7c9aa8cd8b201994d5f5
qkrckstjq <cksk49@naver.com>
2024-07-17 16:25:33 +0900

    feat: RefundsService checkValidRefunds 메소드 추가

--- src/main/java/org/example/springaper/domain/payment/service/RefundsService.java
+++ src/main/java/org/example/springaper/domain/payment/service/RefundsService.java
@@ -7,6 +7,7 @@ import com.siot.IamportRestClient.request.CancelData;
 import com.siot.IamportRestClient.response.IamportResponse;
 import com.siot.IamportRestClient.response.Payment;
 import lombok.RequiredArgsConstructor;
+import org.example.springaper.domain.payment.entity.OrdersDetail;
 import org.example.springaper.domain.user.entity.User;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,8 @@ import javax.net.ssl.HttpsURLConnection;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Service
 @RequiredArgsConstructor
@@ -28,6 +31,16 @@ public class RefundsService {
         IamportResponse<Payment> iamportResponse = iamportClient.cancelPaymentByImpUid(cancelData);
         if (iamportResponse.getCode() != 0) {
             throw new IllegalArgumentException(iamportResponse.getMessage());
+        }
+    }
+    public void checkValidRefunds(List<OrdersDetail> ordersDetailList, User user) {
+        AtomicReference<Long> point = new AtomicReference<>(0L);
+        ordersDetailList.stream()
+                .forEach(ordersDetail -> {
+                    point.updateAndGet(v -> v + ordersDetail.getDigitalProduct().getValue());
+                });
+        if (user.getPoint() < point.get()) {
+            throw new IllegalArgumentException("보유한 포인트가 결제 취소할 포인트 보다 적습니다.");
         }
     }
 }
