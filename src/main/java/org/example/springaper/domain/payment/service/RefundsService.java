b78139400967547212e598681b4a3a69453977bb
qkrckstjq <cksk49@naver.com>
2024-07-17 16:17:25 +0900

    feat: RefundsService postRefundsOrder 메소드 추가

--- src/main/java/org/example/springaper/domain/payment/service/RefundsService.java
+++ src/main/java/org/example/springaper/domain/payment/service/RefundsService.java
@@ -1,11 +1,33 @@
 package org.example.springaper.domain.payment.service;
 
+import com.google.gson.JsonObject;
+import com.siot.IamportRestClient.IamportClient;
+import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.request.CancelData;
+import com.siot.IamportRestClient.response.IamportResponse;
+import com.siot.IamportRestClient.response.Payment;
+import lombok.RequiredArgsConstructor;
 import org.example.springaper.domain.user.entity.User;
 import org.springframework.stereotype.Service;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 @Service
+@RequiredArgsConstructor
 public class RefundsService {
+    private final IamportClient iamportClient;
     public void refundOrder(Long ordersId, User user) {
 
+    }
+
+    public void postRefundsOrder(String impUid) throws IamportResponseException, IOException {
+        CancelData cancelData = new CancelData(impUid, true);
+        IamportResponse<Payment> iamportResponse = iamportClient.cancelPaymentByImpUid(cancelData);
+        if (iamportResponse.getCode() != 0) {
+            throw new IllegalArgumentException(iamportResponse.getMessage());
+        }
     }
 }
