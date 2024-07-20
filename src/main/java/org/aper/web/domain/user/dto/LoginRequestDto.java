c0323ac96765e36fb9b91ca7cd271a259b19419f
weiß <78130010+rosa-eau@users.noreply.github.com>
2024-07-21 05:10:40 +0900

    feat: Signup 관련 dto를 record로 변경 #4

--- src/main/java/org/aper/web/domain/user/dto/LoginRequestDto.java
+++
@@ -1,9 +0,0 @@
-package org.aper.web.domain.user.dto;
-
-import lombok.Getter;
-
-@Getter
-public class LoginRequestDto {
-    private String email;
-    private String password;
-}
---
+++ src/main/java/org/aper/web/domain/user/dto/UserRequestDto.java
@@ -0,0 +1,31 @@
+package org.aper.web.domain.user.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import org.aper.web.domain.user.valid.UserValidationGroup.*;
+
+public class UserRequestDto {
+
+    public record SignupRequestDto(
+            @NotBlank(message = "필명을 입력해주세요.", groups = NicknameBlankGroup.class)
+            @Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,10}$", message = "필명은 특수문자를 제외한 2~10자리를 입력 해주세요.", groups = NicknamePatternGroup.class)
+            String penName,
+            @Schema(description = "이메일", example = "test@email.com")
+            @NotBlank(message = "이메일을 입력해주세요.", groups = EmailBlankGroup.class)
+            @Email(message = "이메일 형식이 아닙니다.", groups = EmailGroup.class)
+            String email,
+            @Schema(description = "비밀번호", example = "Test123!")
+            @NotBlank(message = "비밀번호를 입력해주세요.", groups = PasswordBlankGroup.class)
+            @Pattern(regexp = "^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_~]).{8,15}$",
+                    message = "비밀번호는 영어 소문자, 숫자, 특수문자를 포함한 8~15자리 입니다.", groups = PasswordPatternGroup.class)
+            String password
+            ) {
+    }
+
+    public record LoginRequestDto(
+            String email,
+            String password
+    ){}
+}
