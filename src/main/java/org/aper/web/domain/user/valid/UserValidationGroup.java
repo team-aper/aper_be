4eb7f12a5acec65696cec5a7744f00532bba936d
weiß <78130010+rosa-eau@users.noreply.github.com>
2024-07-21 05:09:11 +0900

    feat: validation Group 생성 #4

---
+++ src/main/java/org/aper/web/domain/user/valid/UserValidationGroup.java
@@ -0,0 +1,27 @@
+package org.aper.web.domain.user.valid;
+
+public class UserValidationGroup {
+    public interface NotBlankGroup {
+    }
+
+    public interface EmailBlankGroup {
+    }
+
+    public interface PasswordBlankGroup {
+    }
+
+    public interface PasswordPatternGroup {
+    }
+
+    public interface NicknameBlankGroup {
+    }
+
+    public interface NicknamePatternGroup {
+    }
+
+    public interface EmailGroup {
+    }
+
+    public interface PatternGroup {
+    }
+}
