6e45a5c8c4496b3bbc16e42a0019897954f60af0
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-07-22 22:47:13 +0900

    fix: HealthCheckController.java 패키지 수정

--- src/main/java/org/aper/web/domain/user/controller/HealthCheckController.java
+++ src/main/java/org/aper/web/domain/user/controller/HealthCheckController.java
@@ -1,4 +1,4 @@
-package org.example.springaper.domain.user.controller;
+package org.aper.web.domain.user.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
