dd3de335e888e5b8158d6820cf30a0a07d208c6a
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-07-22 23:43:30 +0900

    fix: CustomExceptionHandler.java 전체 주석처리

--- src/main/java/org/aper/web/global/exception/CustomExceptionHandler.java
+++ src/main/java/org/aper/web/global/exception/CustomExceptionHandler.java
@@ -1,82 +1,82 @@
-package org.aper.web.global.exception;
+// package org.aper.web.global.exception;
 
 
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.coyote.BadRequestException;
-import org.aper.web.global.dto.ResponseDto;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingRequestCookieException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+// import jakarta.servlet.http.HttpServletRequest;
+// import lombok.extern.slf4j.Slf4j;
+// import org.apache.coyote.BadRequestException;
+// import org.aper.web.global.dto.ResponseDto;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.security.access.AccessDeniedException;
+// import org.springframework.validation.BindingResult;
+// import org.springframework.validation.FieldError;
+// import org.springframework.web.HttpRequestMethodNotSupportedException;
+// import org.springframework.web.bind.MethodArgumentNotValidException;
+// import org.springframework.web.bind.MissingRequestCookieException;
+// import org.springframework.web.bind.annotation.ExceptionHandler;
+// import org.springframework.web.bind.annotation.ResponseStatus;
+// import org.springframework.web.bind.annotation.RestControllerAdvice;
+// import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import java.util.HashMap;
-import java.util.Map;
+// import java.util.HashMap;
+// import java.util.Map;
 
-@Slf4j
-@RestControllerAdvice
-public class CustomExceptionHandler {
+// @Slf4j
+// @RestControllerAdvice
+// public class CustomExceptionHandler {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(BadRequestException.class)
-    public ResponseDto<?> handleBadRequestException(HttpServletRequest request, BadRequestException e) {
-        log.error("handleBadRequestException", e);
-        return ResponseDto.fail(e.getMessage());
-    }
+//     @ResponseStatus(HttpStatus.BAD_REQUEST)
+//     @ExceptionHandler(BadRequestException.class)
+//     public ResponseDto<?> handleBadRequestException(HttpServletRequest request, BadRequestException e) {
+//         log.error("handleBadRequestException", e);
+//         return ResponseDto.fail(e.getMessage());
+//     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseDto<?> handleMethodArgumentNotValidException(HttpServletRequest request, MethodArgumentNotValidException e) {
-        Map<String, String> errorMap = new HashMap<>();
-        BindingResult result = e.getBindingResult();
+//     @ResponseStatus(HttpStatus.BAD_REQUEST)
+//     @ExceptionHandler(MethodArgumentNotValidException.class)
+//     public ResponseDto<?> handleMethodArgumentNotValidException(HttpServletRequest request, MethodArgumentNotValidException e) {
+//         Map<String, String> errorMap = new HashMap<>();
+//         BindingResult result = e.getBindingResult();
 
-        for (FieldError error : result.getFieldErrors()) {
-            errorMap.put(error.getField(), error.getDefaultMessage());
-        }
+//         for (FieldError error : result.getFieldErrors()) {
+//             errorMap.put(error.getField(), error.getDefaultMessage());
+//         }
 
-        log.error("handleMethodArgumentNotValidException", e);
-        return ResponseDto.fail("유효성 검사 실패", errorMap);
-    }
+//         log.error("handleMethodArgumentNotValidException", e);
+//         return ResponseDto.fail("유효성 검사 실패", errorMap);
+//     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseDto<?> handleMethodArgumentTypeMismatchException(HttpServletRequest request, MethodArgumentTypeMismatchException e) {
-        log.error("handleMethodArgumentTypeMismatchException", e);
-        return ResponseDto.fail(e.getMessage());
-    }
+//     @ResponseStatus(HttpStatus.BAD_REQUEST)
+//     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+//     public ResponseDto<?> handleMethodArgumentTypeMismatchException(HttpServletRequest request, MethodArgumentTypeMismatchException e) {
+//         log.error("handleMethodArgumentTypeMismatchException", e);
+//         return ResponseDto.fail(e.getMessage());
+//     }
 
-    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
-    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ResponseDto<?> handleHttpRequestMethodNotSupportedException(HttpServletRequest request, HttpRequestMethodNotSupportedException e) {
-        log.error("handleHttpRequestMethodNotSupportedException", e);
-        return ResponseDto.fail(e.getMessage());
-    }
+//     @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+//     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+//     public ResponseDto<?> handleHttpRequestMethodNotSupportedException(HttpServletRequest request, HttpRequestMethodNotSupportedException e) {
+//         log.error("handleHttpRequestMethodNotSupportedException", e);
+//         return ResponseDto.fail(e.getMessage());
+//     }
 
-    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
-    @ExceptionHandler(AccessDeniedException.class)
-    public ResponseDto<?> handleAccessDeniedException(HttpServletRequest request, AccessDeniedException e) {
-        log.error("handleAccessDeniedException", e);
-        return ResponseDto.fail(e.getMessage());
-    }
+//     @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+//     @ExceptionHandler(AccessDeniedException.class)
+//     public ResponseDto<?> handleAccessDeniedException(HttpServletRequest request, AccessDeniedException e) {
+//         log.error("handleAccessDeniedException", e);
+//         return ResponseDto.fail(e.getMessage());
+//     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MissingRequestCookieException.class)
-    public ResponseDto<?> handleMissingRequestCookieException(HttpServletRequest request, MissingRequestCookieException e) {
-        log.error("handleMissingRequestCookieException", e);
-        return ResponseDto.fail(e.getMessage());
-    }
+//     @ResponseStatus(HttpStatus.BAD_REQUEST)
+//     @ExceptionHandler(MissingRequestCookieException.class)
+//     public ResponseDto<?> handleMissingRequestCookieException(HttpServletRequest request, MissingRequestCookieException e) {
+//         log.error("handleMissingRequestCookieException", e);
+//         return ResponseDto.fail(e.getMessage());
+//     }
 
-    // @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    // @ExceptionHandler(Exception.class)
-    // public ResponseDto<?> handleException(HttpServletRequest request, Exception e) {
-    //     log.error("handleException", e);
-    //     return ResponseDto.fail(ErrorCode.INTERNAL_SERVER_ERROR.getMessage());
-    // }
-}
+//     // @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//     // @ExceptionHandler(Exception.class)
+//     // public ResponseDto<?> handleException(HttpServletRequest request, Exception e) {
+//     //     log.error("handleException", e);
+//     //     return ResponseDto.fail(ErrorCode.INTERNAL_SERVER_ERROR.getMessage());
+//     // }
+// }
