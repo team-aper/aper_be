61e761bfea875d2ccda1b14e98b3a9d5645ad8a9
weiß <78130010+rosa-eau@users.noreply.github.com>
2024-07-22 11:50:36 +0900

    feat: AuthController 생성 #4

---
+++ src/main/java/org/aper/web/global/jwt/controller/AuthController.java
@@ -0,0 +1,46 @@
+package org.aper.web.global.jwt.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import org.aper.web.domain.user.dto.UserRequestDto.LoginRequestDto;
+import org.aper.web.global.dto.ResponseDto;
+import org.aper.web.global.jwt.dto.GeneratedToken;
+import org.aper.web.global.jwt.service.AuthService;
+import org.aper.web.global.jwt.service.CookieService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
+
+@RestController
+public class AuthController {
+
+    private final AuthService authService;
+    private final CookieService cookieService;
+
+    public AuthController(AuthService authService, CookieService cookieService) {
+        this.authService = authService;
+        this.cookieService = cookieService;
+    }
+
+    @PostMapping("/login")
+    public ResponseDto<Void> login(@RequestBody @Valid LoginRequestDto loginRequestDto, HttpServletResponse response) throws UnsupportedEncodingException {
+        GeneratedToken tokens = authService.authenticateAndLogin(loginRequestDto, response);
+        setTokensInResponse(tokens, response);
+        return ResponseDto.success("로그인 성공");
+    }
+
+    @PostMapping("/reissue")
+    public ResponseDto<Void> refresh(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
+        GeneratedToken tokens = authService.reissue(request, response);
+        setTokensInResponse(tokens, response);
+        return ResponseDto.success("토큰 재발급 성공");
+    }
+
+    private void setTokensInResponse(GeneratedToken tokens, HttpServletResponse response) throws UnsupportedEncodingException {
+        response.setHeader("Authorization", tokens.getAccessToken());
+        cookieService.setCookie(response, "refreshToken", tokens.getRefreshToken());
+    }
+}
