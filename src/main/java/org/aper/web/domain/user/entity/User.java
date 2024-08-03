576092a458abd47bc364d71c82d14f4b8df7bc8d
weiß <78130010+rosa-eau@users.noreply.github.com>
2024-08-04 01:12:58 +0900

    feat: 비밀번호 수정 #13

--- src/main/java/org/aper/web/domain/user/entity/User.java
+++ src/main/java/org/aper/web/domain/user/entity/User.java
@@ -48,4 +48,8 @@ public class User {
     public void updatePoint(Long point) {
         this.point += point;
     }
+
+    public void updatePassword(String encodedPassword) {
+        this.password = encodedPassword;
+    }
 }
