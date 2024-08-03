66a2322472bfac136a07789fdca596901e21230f
weiß <78130010+rosa-eau@users.noreply.github.com>
2024-08-04 01:11:49 +0900

    feat: 이메일 인증 관련 코드 추가 #13

---
+++ src/main/java/org/aper/web/domain/user/repository/EmailRepository.java
@@ -0,0 +1,35 @@
+package org.aper.web.domain.user.repository;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.time.Duration;
+
+@Repository
+public class EmailRepository {
+
+    private final StringRedisTemplate redisTemplate;
+
+    public EmailRepository(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    /*이메일 인증 유효기간 5분*/
+    public void saveCertificationNumber(String email, String certificationNumber) {
+        redisTemplate.opsForValue()
+            .set(email, certificationNumber, Duration.ofSeconds(300));
+    }
+
+    public String getCertificationNumber(String email) {
+        return redisTemplate.opsForValue().get(email);
+    }
+
+    public void removeCertificationNumber(String email) {
+        redisTemplate.delete(email);
+    }
+
+    public boolean hashKey(String email) {
+        Boolean keyExists = redisTemplate.hasKey(email);
+        return keyExists != null & Boolean.TRUE.equals(keyExists);
+    }
+}
