5efa4d2fd863fdd431088b7e847bb46ff6678dc5
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-08-05 16:33:52 +0900

    feat: 기 생성된 채팅방 검색을 위한 entity 생성. 채팅방 id에 따른 참여자 string table

---
+++ src/main/java/org/aper/web/domain/chat/entity/ChatRoomView.java
@@ -0,0 +1,20 @@
+package org.aper.web.domain.chat.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Table(name = "chat_room_participants_view")
+@Getter
+public class ChatRoomView {
+
+    @Id
+    private Long chatRoomId;
+    private String participants;
+
+    public ChatRoomView() {
+
+    }
+}
