63159553100f62ec38eab660582cc7b1d16bb97f
qkrckstjq <cksk49@naver.com>
2024-08-08 20:24:25 +0900

    feat: Episode Controller changePublicStatus 메소드 형태 구축

--- src/main/java/org/aper/web/domain/episode/controller/EpisodeController.java
+++ src/main/java/org/aper/web/domain/episode/controller/EpisodeController.java
@@ -1,4 +1,26 @@
 package org.aper.web.domain.episode.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.aper.web.domain.episode.service.EpisodeService;
+import org.aper.web.global.dto.ResponseDto;
+import org.aper.web.global.security.UserDetailsImpl;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/episode")
+@RequiredArgsConstructor
 public class EpisodeController {
+    private final EpisodeService episodeService;
+    @PutMapping("/{episodeId}/publish")
+    public ResponseDto<Void> changePublicStatus(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @PathVariable Long episodeId
+            ) {
+        episodeService.changePublicStatus(episodeId, userDetails);
+        return ResponseDto.success("에피소드의 공개 상태 변경에 성공했습니다.");
+    }
 }
--- src/main/java/org/aper/web/domain/episode/service/EpisodeService.java
+++ src/main/java/org/aper/web/domain/episode/service/EpisodeService.java
@@ -1,4 +1,8 @@
 package org.aper.web.domain.episode.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class EpisodeService {
+
 }
