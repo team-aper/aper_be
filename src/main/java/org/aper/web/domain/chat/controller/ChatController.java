7e94535d8ff3470067e34596aeec7286219e3f87
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-08-11 16:04:54 +0900

    feat: swagger for chat

--- src/main/java/org/aper/web/domain/chat/controller/ChatController.java
+++ src/main/java/org/aper/web/domain/chat/controller/ChatController.java
@@ -1,6 +1,8 @@
 package org.aper.web.domain.chat.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.aper.web.domain.chat.service.ChatService;
 import org.aper.web.global.dto.ResponseDto;
 import org.aper.web.global.security.UserDetailsImpl;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/chat")
+@Tag(name = "chat-controller", description = "to create Chat w. tutor")
 public class ChatController {
 
     private final ChatService chatService;
@@ -22,6 +25,7 @@ public class ChatController {
 
 
     @PostMapping("/{tutorId}")
+    @Operation(summary = "튜터와 채팅방 만들기", description = "이미 생성된 채팅방인지 확인하고, 생성되어 있지 않다면 채팅방 생성")
     public ResponseDto<Void> createChat(@PathVariable Long tutorId,
                                         @AuthenticationPrincipal UserDetailsImpl userDetails) {
         Long userId = userDetails.user().getUserId();
