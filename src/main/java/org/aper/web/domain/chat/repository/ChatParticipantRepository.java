2fc37440fed9297224cb3ff9bceea8101f95a1c6
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-08-12 14:53:59 +0900

    feat: 튜터가 맞는지, 유저 아이디가 일치하는지, 채팅방 아이디가 일치하는 지에 따라 채팅방 반환

--- src/main/java/org/aper/web/domain/chat/repository/ChatParticipantRepository.java
+++ src/main/java/org/aper/web/domain/chat/repository/ChatParticipantRepository.java
@@ -5,10 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ChatParticipantRepository extends JpaRepository<ChatParticipant, Long> {
     List<ChatParticipant> findByChatRoomId(Long chatRoomId);
 
     List<ChatParticipant> findByUserUserId(Long userId);
+
+    List<ChatParticipant> findByIsTutorAndUserUserId(boolean b, Long tutorId);
+
+    Optional<ChatParticipant> findByIsTutorAndUserUserIdAndChatRoomId(boolean b, Long tutorId, Long roomId);
 }
