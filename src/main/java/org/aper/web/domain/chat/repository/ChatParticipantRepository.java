435f426ce910bcdb2aa9597d38fc6b31bdb53f2e
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-08-12 15:17:35 +0900

    refactor: deletion of unnecessary codes

--- src/main/java/org/aper/web/domain/chat/repository/ChatParticipantRepository.java
+++ src/main/java/org/aper/web/domain/chat/repository/ChatParticipantRepository.java
@@ -9,11 +9,8 @@ import java.util.Optional;
 
 @Repository
 public interface ChatParticipantRepository extends JpaRepository<ChatParticipant, Long> {
-    List<ChatParticipant> findByChatRoomId(Long chatRoomId);
 
     List<ChatParticipant> findByUserUserId(Long userId);
-
-    List<ChatParticipant> findByIsTutorAndUserUserId(boolean b, Long tutorId);
 
     Optional<ChatParticipant> findByIsTutorAndUserUserIdAndChatRoomId(boolean b, Long tutorId, Long roomId);
 }
