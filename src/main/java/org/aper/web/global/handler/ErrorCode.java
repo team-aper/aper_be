a2a1b7da4faa08365a118edd87850ad6dc21b958
weiß <78130010+rosa-eau@users.noreply.github.com>
2024-08-21 00:47:46 +0900

    feat: ErrorCode 수정

--- src/main/java/org/aper/web/global/handler/ErrorCode.java
+++ src/main/java/org/aper/web/global/handler/ErrorCode.java
@@ -15,11 +15,11 @@ public enum ErrorCode {
     REFRESH_TOKEN_NOT_EXISTS("A002", "리프레시 토큰을 찾을 수 없습니다."),
     INVALID_REFRESH_TOKEN("A003", "유효하지 않은 리프레시 토큰입니다."),
     INVALID_ACCESS_TOKEN("A004", "유효하지 않은 엑세스 토큰입니다."),
-    EXPIRED_REFRESH_TOKEN("A006", "만료된 리프레시 토큰입니다."),
-    EXPIRED_ACCESS_TOKEN("A007", "만료된 엑세스 토큰입니다."),
-    ACCESS_TOKEN_IS_NULL("A008", "엑세스 토큰이 존재하지 않습니다."),
-    BLACK_LISTED_TOKEN("A009", "블랙리스트에 등록된 토큰입니다."),
-    AUTH_NOT_FOUND("A010", "사용자의 권한을 찾을 수 없습니다."),
+    EXPIRED_REFRESH_TOKEN("A005", "만료된 리프레시 토큰입니다."),
+    EXPIRED_ACCESS_TOKEN("A006", "만료된 엑세스 토큰입니다."),
+    ACCESS_TOKEN_IS_NULL("A007", "엑세스 토큰이 존재하지 않습니다."),
+    BLACK_LISTED_TOKEN("A008", "블랙리스트에 등록된 토큰입니다."),
+    AUTH_NOT_FOUND("A009", "사용자의 권한을 찾을 수 없습니다."),
 
     // User
     ALREADY_EXIST_EMAIL("U001", "이미 가입된 이메일입니다."),
