fc3ac807f26cdf6c478452314b897a1a706f6ac0
qkrckstjq <cksk49@naver.com>
2024-08-21 13:40:18 +0900

    feat: deleteAccount 삭제 계정 테이블 추가

---
+++ src/main/java/org/aper/web/domain/user/entity/DeleteAccount.java
@@ -0,0 +1,16 @@
+package org.aper.web.domain.user.entity;
+
+import jakarta.persistence.*;
+import org.aper.web.global.entity.BaseSoftDeleteEntity;
+
+@Entity
+public class DeleteAccount extends BaseSoftDeleteEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @OneToOne
+    @JoinColumn
+    private User user;
+}
