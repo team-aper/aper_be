628e75a3f021351ffc23844cc5e60e3c58883b68
qkrckstjq <cksk49@naver.com>
2024-08-28 14:15:41 +0900

    feat: findByPenName 쿼리 추가

--- src/main/java/org/aper/web/domain/search/service/SearchService.java
+++ src/main/java/org/aper/web/domain/search/service/SearchService.java
@@ -1,8 +1,21 @@
 package org.aper.web.domain.search.service;
 
+import lombok.RequiredArgsConstructor;
+import org.aper.web.domain.search.dto.SearchDto;
+import org.aper.web.domain.user.entity.User;
+import org.aper.web.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class SearchService {
+    private final UserRepository userRepository;
 
+    public SearchDto.SearchStoryResponseDto getSearchStory(int page, int size, String storyTitle, String episodeTitle, String episodeParagraph) {
+
+    }
+
+    public SearchDto.SearchAuthorResponseDto getSearchAuthor(String penName) {
+        User targetAuthor = userRepository.findBy
+    }
 }
