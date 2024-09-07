9e3037c72e33617cf1b73a856158cbabd45b359b
qkrckstjq <cksk49@naver.com>
2024-09-07 16:40:29 +0900

    feat: elastic 관련 에러 코드 추가

--- src/main/java/org/aper/web/global/handler/ErrorCode.java
+++ src/main/java/org/aper/web/global/handler/ErrorCode.java
@@ -45,8 +45,11 @@ public enum ErrorCode {
     // AWS S3
     S3_UPLOAD_ERROR_OCCURRED("AWS001", "S3 업로드 중 에러가 발생했습니다."),
 
-    // json
-    JSON_PROCESSING_ERROR("JS001", "JSON 변환 중 에러 발생");
+    // elastic
+    JSON_PROCESSING_ERROR("ES001", "JSON 변환 중 에러 발생"),
+    DOCUMENT_UPDATE_FAILED("ES002", "엘라스틱 서치 데이터 업데이트에 실패했습니다."),
+    DOCUMENT_DELETE_FAILED("ES003", "엘라스틱 서치 데이터 삭제에 실패했습니다."),
+    ELASTICSEARCH_CONNECT_FAILED("ES004", "엘라스틱 서치에 요청을 실패했습니다.");
 
     private final String code;
     private final String message;
