89ab169d3797193053529f15dfa39781a10a8a0d
qkrckstjq <cksk49@naver.com>
2024-09-11 22:35:55 +0900

    feat: SearchController에 필명API, 작가검색API 추가 및 swagger에 설명 추가

--- src/main/java/org/aper/web/global/docs/SearchControllerDocs.java
+++ src/main/java/org/aper/web/global/docs/SearchControllerDocs.java
@@ -2,7 +2,9 @@ package org.aper.web.global.docs;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.aper.web.domain.search.entity.dto.SearchDto;
+import org.aper.web.domain.search.entity.dto.SearchDto.SearchAuthorResponseDto;
+import org.aper.web.domain.search.entity.dto.SearchDto.SearchPenNameResponseDto;
+import org.aper.web.domain.search.entity.dto.SearchDto.SearchStoryResponseDto;
 import org.aper.web.domain.story.entity.constant.StoryGenreEnum;
 import org.aper.web.global.dto.ResponseDto;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,21 +19,42 @@ public interface SearchControllerDocs {
                     "에피소드의 내용에 포함되었다면 해당 내용에서 검색했던 단어를 기준으로 앞 40글자 뒤 40글자를 전달합니다.<br>" +
                     "<h4> filter값은 필수 입력값 입니다. <h4>"
     )
-            ResponseDto<SearchDto.SearchStoryResponseDto> getSearchStory(
+            ResponseDto<SearchStoryResponseDto> getSearchStory(
             @RequestParam(required = false, defaultValue = "0") int page,
             @RequestParam(required = false, defaultValue = "10") int size,
             @RequestParam(required = false) StoryGenreEnum genre,
             @RequestParam String filter
     );
 
-    @Operation(summary = "작가 필명을 기반으로 검색할때의 API",
+    @Operation(summary = "작가 필명만 검색할때 API",
             description = "page와 size는 입력하지 않을 시 각각 0, 10이 기본값입니다.<br>" +
-                    "검색 문자를 포함한 모든 작가를 탐색합니다." +
-                    "<h4> penName값은 필수 입력값 입니다. <h4>"
+                    "검색 문자를 포함한 모든 작가를 탐색합니다.<br>" +
+                    "<h4> 입력시 자동완성 기능같은 경우의 API입니다. 필명만 보냅니다 <h4>"
     )
-    ResponseDto<SearchDto.SearchAuthorResponseDto> getSearchAuthor(
+    ResponseDto<SearchPenNameResponseDto> getSearchAuthorOnlyPenName(
             @RequestParam(required = false, defaultValue = "0") int page,
             @RequestParam(required = false, defaultValue = "10") int size,
             @RequestParam String penName
     );
+
+    @Operation(summary = "작가 필명을 기반으로 검색할때의 API",
+            description = "page와 size는 입력하지 않을 시 각각 0, 10이 기본값입니다.<br>" +
+                    "검색 문자를 포함한 모든 작가를 탐색합니다." +
+                    "<h4>userId, penName 등등 user와 관련된 추가정보까지 한번에 보냅니다.<h4>"
+    )
+    ResponseDto<SearchAuthorResponseDto> getSearchAuthor(
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @RequestParam(required = false, defaultValue = "10") int size,
+            @RequestParam String penName
+    );
+
+    @Operation(summary = "mysql의 Episode테이블과 엘라스틱서치의 싱크를 맞추는 API",
+            description = "실제 서비스에서는 사용되지 않는 개발용 API 입니다."
+    )
+    ResponseDto<Void> testSyncEpisode();
+
+    @Operation(summary = "mysql의 User테이블과 엘라스틱서치의 싱크를 맞추는 API",
+            description = "실제 서비스에서는 사용되지 않는 개발용 API 입니다."
+    )
+    ResponseDto<Void> testSyncUser();
 }
