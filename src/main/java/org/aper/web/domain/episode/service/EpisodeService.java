a59a138f69dd28a39f4003edaa0bc128fc69aacf
weiß <78130010+rosa-eau@users.noreply.github.com>
2024-10-02 02:40:49 +0900

    feat: Modify EpisodeService for batch #71

--- src/main/java/org/aper/web/domain/episode/service/EpisodeService.java
+++ src/main/java/org/aper/web/domain/episode/service/EpisodeService.java
@@ -2,10 +2,9 @@ package org.aper.web.domain.episode.service;
 
 import lombok.RequiredArgsConstructor;
 import org.aper.web.domain.episode.dto.EpisodeRequestDto.DeleteEpisodeDto;
-import org.aper.web.domain.episode.dto.EpisodeRequestDto.TextChangeDto;
 import org.aper.web.domain.episode.dto.EpisodeRequestDto.TitleChangeDto;
-import org.aper.web.domain.episode.dto.EpisodeResponseDto.*;
 import org.aper.web.domain.episode.dto.EpisodeResponseDto.EpisodeHeaderDto;
+import org.aper.web.domain.episode.dto.EpisodeResponseDto.EpisodeTextDto;
 import org.aper.web.domain.episode.entity.Episode;
 import org.aper.web.domain.episode.repository.EpisodeRepository;
 import org.aper.web.domain.kafka.service.KafkaEpisodesProducerService;
@@ -48,17 +47,6 @@ public class EpisodeService {
     }
 
     @Transactional
-    public void changeText(UserDetailsImpl userDetails, Long episodeId, TextChangeDto textChangeDto) {
-        Episode episode = episodeHelper.validateEpisodeExists(episodeId);
-        episodeHelper.validateUserIsAuthor(episode, userDetails);
-
-        episode.updateText(textChangeDto.text());
-        episodeRepository.save(episode);
-
-        producerService.sendUpdate(episode);
-    }
-
-    @Transactional
     public void deleteEpisode(UserDetailsImpl userDetails, Long episodeId, DeleteEpisodeDto episodeDto) {
         Episode episode = episodeHelper.validateEpisodeExists(episodeId);
         episodeHelper.validateUserIsAuthor(episode, userDetails);
@@ -85,13 +73,13 @@ public class EpisodeService {
     public EpisodeTextDto getEpisodeText(UserDetailsImpl userDetails, Long episodeId) {
         Episode episode = episodeHelper.validateEpisodeExists(episodeId);
         if (storyHelper.isOwnStory(episode.getStory().getId(), userDetails)){
-            return new EpisodeTextDto(episode.getDescription());
+            return new EpisodeTextDto(episode.getParagraphs());
         }
 
         if (!episode.isOnDisplay()){
             throw new ServiceException(ErrorCode.EPISODE_NOT_PUBLISHED);
         }
 
-        return new EpisodeTextDto(episode.getDescription());
+        return new EpisodeTextDto(episode.getParagraphs());
     }
 }
