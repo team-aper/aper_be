4f4331fb039b1cd63907ec62165b5f11bb086b1d
weiß <78130010+rosa-eau@users.noreply.github.com>
2024-10-02 20:24:58 +0900

    feat: delete Garbage #71

--- src/main/java/org/aper/web/domain/paragraph/dto/BatchPayload.java
+++
@@ -1,9 +0,0 @@
-package org.aper.web.domain.paragraph.dto;
-
-import java.util.List;
-
-public class BatchPayload {
-    private List<ParagraphRequestDto.ParagraphDto> modified;
-    private List<ParagraphRequestDto.ParagraphDto> added;
-    private List<String> deleted;
-}
