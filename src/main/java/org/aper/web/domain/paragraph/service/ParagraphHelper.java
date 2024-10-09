fc2020d2ad1100d70499c38aad6b3ad0707ee623
weiß <78130010+rosa-eau@users.noreply.github.com>
2024-10-10 05:34:15 +0900

    fix: Resolve handle exception with DELETE batch operations #81

--- src/main/java/org/aper/web/domain/paragraph/service/ParagraphHelper.java
+++ src/main/java/org/aper/web/domain/paragraph/service/ParagraphHelper.java
@@ -10,6 +10,8 @@ import org.aper.web.global.handler.exception.ServiceException;
 import org.aper.web.global.security.UserDetailsImpl;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 public class ParagraphHelper {
@@ -45,14 +47,41 @@ public class ParagraphHelper {
         }
     }
 
-    public boolean isFirstParagraph(String previousUuid) {
-        return previousUuid == null;
-    }
-
     public String truncateParagraph(String paragraph) {
         if (paragraph == null) {
             return null;
         }
         return paragraph.length() > 250 ? paragraph.substring(0, 250) + "..." : paragraph + "...";
+    }
+
+    public void updateEpisodeDescription(Episode episode) {
+        List<Paragraph> paragraphs = episode.getParagraphs();
+
+        Paragraph firstParagraph = paragraphs.stream()
+                .filter(paragraph -> paragraph.getPreviousUuid() == null)
+                .findFirst()
+                .orElseThrow(() -> new ServiceException(ErrorCode.PARAGRAPH_NOT_FOUND));
+
+        String truncatedParagraph = truncateParagraph(firstParagraph.getContent());
+        episode.updateDescription(truncatedParagraph);
+        episodeRepository.save(episode);
+    }
+
+    public void updatePreviousParagraph(String previousUuid, String nextUuid, List<Paragraph> paragraphsToUpdate) {
+        if (previousUuid != null) {
+            Paragraph previousParagraph = paragraphRepository.findByUuid(previousUuid)
+                    .orElseThrow(() -> new ServiceException(ErrorCode.PARAGRAPH_NOT_FOUND));
+            previousParagraph.updateNextUuid(nextUuid);
+            paragraphsToUpdate.add(previousParagraph);
+        }
+    }
+
+    public void updateNextParagraph(String previousUuid, String nextUuid, List<Paragraph> paragraphsToUpdate) {
+        if (nextUuid != null) {
+            Paragraph nextParagraph = paragraphRepository.findByUuid(nextUuid)
+                    .orElseThrow(() -> new ServiceException(ErrorCode.PARAGRAPH_NOT_FOUND));
+            nextParagraph.updatePreviousUuid(previousUuid);
+            paragraphsToUpdate.add(nextParagraph);
+        }
     }
 }
