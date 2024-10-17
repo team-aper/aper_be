8ac47c52d138c44c884ba297d346f5a82284c808
weiß <78130010+rosa-eau@users.noreply.github.com>
2024-10-18 03:58:38 +0900

    feat: Add Query about counting Subscriptions #80

--- src/main/java/org/aper/web/domain/subscription/repository/SubscriptionRepository.java
+++ src/main/java/org/aper/web/domain/subscription/repository/SubscriptionRepository.java
@@ -3,6 +3,7 @@ package org.aper.web.domain.subscription.repository;
 import org.aper.web.domain.subscription.entity.Subscription;
 import org.aper.web.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +14,9 @@ public interface SubscriptionRepository extends JpaRepository<Subscription, Long
     List<Subscription> findAllByAuthor_UserId(Long authorId);
 
     boolean existsBySubscriberAndAuthor(User subscriber, User author);
+
+    @Query("SELECT COUNT(s) FROM Subscription s WHERE s.author.userId = :authorId")
+    Long countByAuthorId(Long authorId);
+
 
 }
