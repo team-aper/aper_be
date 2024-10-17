0a36ba11e85ecbac219805c697bf73be6434efe9
weiß <78130010+rosa-eau@users.noreply.github.com>
2024-10-18 02:34:50 +0900

    feat: Add Repository about Subscription #80

--- src/main/java/org/aper/web/domain/subscription/repository/SubscriptionRepository.java
+++ src/main/java/org/aper/web/domain/subscription/repository/SubscriptionRepository.java
@@ -1,6 +1,7 @@
 package org.aper.web.domain.subscription.repository;
 
 import org.aper.web.domain.subscription.entity.Subscription;
+import org.aper.web.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +10,8 @@ import java.util.List;
 @Repository
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
 
-    List<Subscription> findAllBySubscriber_UserId(Long subscriberId);
-
     List<Subscription> findAllByAuthor_UserId(Long authorId);
+
+    boolean existsBySubscriberAndAuthor(User subscriber, User author);
+
 }
