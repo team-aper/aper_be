5e2337b699d8d697078364f2e5d78da6216ad1cb
weiß <78130010+rosa-eau@users.noreply.github.com>
2024-10-18 02:33:56 +0900

    feat: Add ResponseDto about Subscription #80

---
+++ src/main/java/org/aper/web/domain/subscription/dto/SubscriptionResponseDto.java
@@ -0,0 +1,52 @@
+package org.aper.web.domain.subscription.dto;
+
+import org.aper.web.domain.story.dto.StoryResponseDto.GetRecommendedStory;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class SubscriptionResponseDto {
+
+    public record IsSubscribed(
+            boolean isSubscribed
+    ){}
+
+    public record SubscribedAuthors(
+            List<SubscribedAuthor> authors
+    ){}
+
+    public record AuthorRecommendations(
+            List<AuthorRecommendation> daily,
+            List<AuthorRecommendation> romance,
+            List<AuthorRecommendation> horror,
+            List<AuthorRecommendation> sf,
+            List<AuthorRecommendation> queer,
+            List<AuthorRecommendation> society,
+            List<AuthorRecommendation> art,
+            List<AuthorRecommendation> criticism,
+            List<AuthorRecommendation> poetry
+    ){}
+
+    public record SubscribedAuthor(
+            String authorPenName,
+            String fieldImageUrl,
+            Long authorId,
+            Long chapter,
+            Long storyId,
+            String storyTitle,
+            String genre,
+            LocalDate publicDate,
+            String episodeId,
+            String episodeTitle,
+            String description,
+            boolean isRead
+    ){}
+
+    public record AuthorRecommendation(
+            String penName,
+            String fieldImageUrl,
+            String description,
+            Long authorId,
+            List<GetRecommendedStory> storyList
+    ){}
+}
