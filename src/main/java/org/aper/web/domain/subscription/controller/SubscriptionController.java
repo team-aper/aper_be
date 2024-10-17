674d16a3ad1ab3449c9080f3f299c1a15224088b
weiß <78130010+rosa-eau@users.noreply.github.com>
2024-10-18 02:34:44 +0900

    feat: Add Controller about Subscription #80

--- src/main/java/org/aper/web/domain/subscription/controller/SubscriptionController.java
+++ src/main/java/org/aper/web/domain/subscription/controller/SubscriptionController.java
@@ -1,18 +1,17 @@
 package org.aper.web.domain.subscription.controller;
 
+import org.aper.web.domain.subscription.dto.SubscriptionResponseDto.AuthorRecommendations;
+import org.aper.web.domain.subscription.dto.SubscriptionResponseDto.IsSubscribed;
+import org.aper.web.domain.subscription.dto.SubscriptionResponseDto.SubscribedAuthors;
 import org.aper.web.domain.subscription.service.SubscriptionService;
-import org.aper.web.domain.user.entity.User;
 import org.aper.web.global.dto.ResponseDto;
+import org.aper.web.global.security.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/subscriptions")
+@RequestMapping("/subscribe")
 public class SubscriptionController {
 
     private final SubscriptionService subscriptionService;
@@ -22,9 +21,28 @@ public class SubscriptionController {
         this.subscriptionService = subscriptionService;
     }
 
-    @GetMapping("/user/{userId}")
-    public ResponseDto<List<User>> getSubscribedAuthors(@PathVariable Long userId) {
-        List<User> subscribedAuthors = subscriptionService.getSubscribedAuthors(userId);
-        return ResponseDto.success("Author data", subscribedAuthors);
+    @PostMapping("/{authorId}")
+    public ResponseDto<Void> subscribe(@PathVariable Long authorId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        subscriptionService.subscribe(userDetails, authorId);
+        return ResponseDto.success("Subscribed successfully");
     }
+
+    @GetMapping("/is-subscribed/{authorId}")
+    public ResponseDto<IsSubscribed> isSubscribed(@PathVariable Long authorId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        IsSubscribed isSubscribed = subscriptionService.isSubscribed(userDetails, authorId);
+        return ResponseDto.success("is Subscribed data", isSubscribed);
+    }
+
+    @GetMapping("/subscribed-authors")
+    public ResponseDto<SubscribedAuthors> getSubscribedAuthors(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        SubscribedAuthors subscribedAuthors = subscriptionService.getSubscribedAuthors(userDetails);
+        return ResponseDto.success("Subscribed Authors Data", subscribedAuthors);
+    }
+
+    @GetMapping("/recommended-authors")
+    public ResponseDto<AuthorRecommendations> getRecommendedAuthors() {
+        AuthorRecommendations recommendedAuthors = subscriptionService.getRecommendedAuthors();
+        return ResponseDto.success("Recommended Authors Data", recommendedAuthors);
+    }
+
 }
