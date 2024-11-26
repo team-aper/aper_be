d6473d299f4028af2f674670fe2097a278bd57b2
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-11-26 19:01:06 +0900

    refactor: chat feat deleted in the main server

--- src/main/java/org/aper/web/domain/chat/repository/ChatParticipantRepository.java
+++ src/main/java/org/aper/web/domain/chat/repository/ChatParticipantRepository.java
@@ -10,9 +10,5 @@ import java.util.Optional;
 @Repository
 public interface ChatParticipantRepository extends JpaRepository<ChatParticipant, Long> {
 
-    List<ChatParticipant> findByUserUserId(Long userId);
-
-    Optional<ChatParticipant> findByIsTutorAndUserUserIdAndChatRoomId(boolean b, Long tutorId, Long roomId);
-
     Long countByUserUserIdAndIsTutorTrue(Long userId);
 }
