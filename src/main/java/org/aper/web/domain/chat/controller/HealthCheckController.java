8727b84d80218863af4c0ccaccfb02a907cf3dda
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-11-26 18:57:33 +0900

    feat: auth healthcheck controller added

--- src/main/java/org/aper/web/domain/chat/controller/HealthCheckController.java
+++ src/main/java/org/aper/web/domain/chat/controller/HealthCheckController.java
@@ -1,5 +1,7 @@
 package org.aper.web.domain.chat.controller;
 
+import org.aper.web.global.security.UserDetailsImpl;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +12,8 @@ public class HealthCheckController {
     public String checkHealth() {
         return "OK";
     }
+
+    @GetMapping("/health/auth")
+    public String authHealthCheck(@AuthenticationPrincipal UserDetailsImpl userDetails) { return userDetails.getUsername(); }
+
 }
