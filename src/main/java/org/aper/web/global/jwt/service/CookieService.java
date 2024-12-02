f546eb38fdc3ac63ea86404922b79573331149dd
weiß <78130010+rosa-eau@users.noreply.github.com>
2024-12-02 22:00:13 +0900

    test: change setSecure false for local test #138

--- src/main/java/org/aper/web/global/jwt/service/CookieService.java
+++ src/main/java/org/aper/web/global/jwt/service/CookieService.java
@@ -17,7 +17,7 @@ public class CookieService {
         if (cookie != null) {
             cookie.setValue("");
             cookie.setPath("/");
-            cookie.setSecure(true);
+            cookie.setSecure(false);
             cookie.setAttribute("SameSite", "None");
             cookie.setMaxAge(0);
             cookie.setHttpOnly(true);
@@ -28,7 +28,7 @@ public class CookieService {
     @NotNull
     public Cookie createCookie(String key, String value) {
         Cookie cookie = new Cookie(key, value);
-        cookie.setSecure(true);
+        cookie.setSecure(false);
         cookie.setHttpOnly(true);
         cookie.setPath("/");
         cookie.setAttribute("SameSite", "None");
