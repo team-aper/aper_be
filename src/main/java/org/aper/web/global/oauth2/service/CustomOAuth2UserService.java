21a94911d9e4a4adba42778a2474865ea1cc0b9a
Weiß <tory020900@gmail.com>
2024-12-09 19:53:33 +0900

    fix: Add OAuthProxyController #142

--- src/main/java/org/aper/web/global/oauth2/service/CustomOAuth2UserService.java
+++ src/main/java/org/aper/web/global/oauth2/service/CustomOAuth2UserService.java
@@ -1,4 +1,4 @@
-package org.aper.web.global.oauth2;
+package org.aper.web.global.oauth2.service;
 
 import com.aperlibrary.user.entity.User;
 import com.aperlibrary.user.entity.constant.UserRoleEnum;
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.aper.web.domain.user.repository.UserRepository;
 import org.aper.web.global.handler.ErrorCode;
 import org.aper.web.global.handler.exception.ServiceException;
+import org.aper.web.global.oauth2.OAuth2Attribute;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
