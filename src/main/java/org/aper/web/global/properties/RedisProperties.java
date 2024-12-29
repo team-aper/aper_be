64c0f109e2f9e43e3581650fcb7809d0fbe2e781
Weiß <tory020900@gmail.com>
2024-12-29 17:29:33 +0900

    feat: Use master node and add ssl enabled true #146

--- src/main/java/org/aper/web/global/properties/RedisProperties.java
+++ src/main/java/org/aper/web/global/properties/RedisProperties.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Getter
 @Setter
 @Component
-@ConfigurationProperties(prefix = "spring.redis")
+@ConfigurationProperties(prefix = "spring.data.redis")
 public class RedisProperties {
     private String host;
     private int port;
